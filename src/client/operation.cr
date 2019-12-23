2ea7bbaca8c82aaddb6c00916e43714f42a06c1c
Julien Reichardt <git@jrei.ch>
2019-12-23 13:30:32 +0100

    Fix Operation#wait timeout

--- src/client/operation.cr
+++ src/client/operation.cr
@@ -7,9 +7,11 @@ struct CrystaLXD::Operation
     @client.api_path = @client.api_path + "/operations/"
   end
 
-  # Wait for an operation to finish. Can take as arguments an UUID as a string, and a timeout in seconds.
+  # Wait for an operation to finish, with an optional timeout in seconds
   # (https://github.com/lxc/lxd/blob/master/doc/rest-api.md#get-optional-timeout30).
-  def wait(timeout : Int32 = 30) : Success(BackgroundOperation) | Error
-    @client.get BackgroundOperation, uuid + "/wait"
+  def wait(timeout : Int32? = nil) : Success(BackgroundOperation) | Error
+    path = uuid + "/wait"
+    path += "?timeout=#{timeout}" if timeout
+    @client.get BackgroundOperation, path
   end
 end
